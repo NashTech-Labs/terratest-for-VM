d260a669861228e9c6cfd3a026b10f226e8ec718
muzakkir <mohd.saifi@nashtechglobal.com>
2023-08-31 17:36:53 +0530

    Few updates

--- README.md
+++ README.md
@@ -25,16 +25,16 @@
 
  
 
-        export ARM_CLIENT_ID=""
+        export CLIENT_ID=""
 
  
 
-        export ARM_CLIENT_SECRET=""
+        export CLIENT_SECRET=""
 
  
 
-        export ARM_TENANT_ID=""
+        export TENANT_ID=""
 
  
 
-        export ARM_SUBSCRIPTION_ID=""
+        export SUBSCRIPTION_ID=""
--- main_test.go
+++ main_test.go
@@ -1,322 +1,230 @@
 package main
 
-
-
-
 import (
+	"encoding/json"
 
-    "encoding/json"
+	"fmt"
 
-    "fmt"
+	"io/ioutil"
 
-    "io/ioutil"
+	"net/http"
 
-    "net/http"
+	"os"
 
-    "os"
+	"os/exec"
 
-    "os/exec"
+	"strings"
 
-    "strings"
+	"testing"
 
-    "testing"
+	"github.com/gruntwork-io/terratest/modules/terraform"
 
-    "github.com/gruntwork-io/terratest/modules/terraform"
-
-    "github.com/stretchr/testify/assert"
-
+	"github.com/stretchr/testify/assert"
 )
-
 
 var (
+	apiVersion = "2023-03-01"
 
-    apiVersion                  = "2023-03-01"
-
-    expected_provisioningState  = "Succeeded" // vm status
-
+	expected_provisioningState = "Succeeded" // vm status
 
 )
-
-
-
 
 // const (
 
-//  poolID = 53
+//  poolIDMK = 53
 
 // )
 
-
-
-
 func TestAzureVMWithLogic(t *testing.T) {
 
-    t.Parallel()
+	t.Parallel()
 
+	terraformOptions := &terraform.Options{
 
+		TerraformDir: "../module",
 
+		Vars: map[string]interface{}{
 
-    terraformOptions := &terraform.Options{
+			"vm_size": "Standard_DS1_v2",
+		},
+	}
 
-        TerraformDir: "../module",
+	defer terraform.Destroy(t, terraformOptions)
 
-        Vars: map[string]interface{}{
+	terraform.InitAndApply(t, terraformOptions)
 
-            "vm_size": "Standard_DS1_v2",
+	// getting resource group name from the output.tf file
 
-        },
+	resourceGroupName := terraform.Output(t, terraformOptions, "resource_group_name")
 
-    }
+	// fmt.Println("Resource Group Name:", resourceGroupName)
 
+	// getting subscription ID from the Environment variable
 
+	subscriptionID := os.Getenv("SUBSCRIPTION_ID")
 
+	// fmt.Println("THis is your subscription ID:-", subscriptionID)
 
-    defer terraform.Destroy(t, terraformOptions)
+	if subscriptionID == "" {
 
+		t.Fatal("AZURE_SUBSCRIPTION_ID environment variable is not set")
 
+	}
 
+	// getting token ID from the Environment variable for ADO
 
-    terraform.InitAndApply(t, terraformOptions)
+	personalAccessTokenMK := os.Getenv("TF_VAR_adotoken")
 
+	// fmt.Println("Personal Access Token for the ADO:-", personalAccessTokenMK)
 
+	if personalAccessTokenMK == "" {
 
+		t.Fatal("AZURE_PERSONAL_ACCESS_TOKEN environment variable is not set")
 
-    // getting resource group name from the output.tf file
+	}
 
-    resourceGroupName := terraform.Output(t, terraformOptions, "resource_group_name")
+	// getting token using the subscriptionID
 
-    // fmt.Println("Resource Group Name:", resourceGroupName)
+	accessToken, err := getAccessToken(subscriptionID)
 
+	// fmt.Println(accessToken)
 
+	if err != nil {
 
+		t.Errorf("Failed to get access token: %s", err.Error())
 
-    // getting subscription ID from the Environment variable
+		return
 
-    subscriptionID := os.Getenv("ARM_SUBSCRIPTION_ID")
+	}
 
-    // fmt.Println("THis is your subscription ID:-", subscriptionID)
+	// FIRST TEST CASE:- Testing the Vm status..
 
-    if subscriptionID == "" {
+	// getting vm-name from the output.tf file and run the test on all the vm.
 
-        t.Fatal("AZURE_SUBSCRIPTION_ID environment variable is not set")
+	vmNames := terraform.OutputList(t, terraformOptions, "vm_names")
 
-    }
+	for _, name := range vmNames {
 
-    // getting token ID from the Environment variable for ADO
+		// fmt.Println(name)
 
-    personalAccessToken := os.Getenv("TF_VAR_adotoken")
+		url := fmt.Sprintf("https://management.azure.com/subscriptions/%s/resourceGroups/%s/providers/Microsoft.Compute/virtualMachines/%s?api-version=%s", subscriptionID, resourceGroupName, name, apiVersion)
 
-    // fmt.Println("Personal Access Token for the ADO:-", personalAccessToken)
+		// fmt.Println(url)
 
-    if personalAccessToken == "" {
+		vmJSON, err := getVirtualMachineDetails(url, accessToken)
 
-        t.Fatal("AZURE_PERSONAL_ACCESS_TOKEN environment variable is not set")
+		// fmt.Println(vmJSON)
 
-    }
+		if err != nil {
 
-    // getting token using the subscriptionID
+			t.Errorf("Failed to get virtual machine details: %s", err.Error())
 
-    accessToken, err := getAccessToken(subscriptionID)
+			return
 
-    // fmt.Println(accessToken)
+		}
 
-    if err != nil {
+		vmData, err := printVirtualMachineDetails([]byte(vmJSON))
 
-        t.Errorf("Failed to get access token: %s", err.Error())
+		// fmt.Println(vmData)
 
-        return
+		if err != nil {
 
-    }
+			fmt.Println("Error:", err)
 
+			return
 
+		}
 
+		actual_provisioningState, ok := vmData["properties"].(map[string]interface{})["provisioningState"].(string)
 
+		// fmt.Println(actual_provisioningState)
 
+		if !ok {
 
-// FIRST TEST CASE:- Testing the Vm status..
+			t.Errorf("Failed to get provisioning state for VM %s", name)
 
+			return
 
+		}
 
+		t.Run(fmt.Sprintf("State has been matched for VM: %s", name), func(t *testing.T) {
 
-    // getting vm-name from the output.tf file and run the test on all the vm.
+			assert.Equal(t, expected_provisioningState, actual_provisioningState, fmt.Sprintf("State has been mis-matched for VM: %s", name))
 
-    vmNames := terraform.OutputList(t, terraformOptions, "vm_names")
+		})
 
-    for _, name := range vmNames {
-
-        // fmt.Println(name)
-
-
-
-
-        url := fmt.Sprintf("https://management.azure.com/subscriptions/%s/resourceGroups/%s/providers/Microsoft.Compute/virtualMachines/%s?api-version=%s", subscriptionID, resourceGroupName, name, apiVersion)
-
-        // fmt.Println(url)
-
-
-
-
-        vmJSON, err := getVirtualMachineDetails(url, accessToken)
-
-        // fmt.Println(vmJSON)
-
-
-
-
-        if err != nil {
-
-            t.Errorf("Failed to get virtual machine details: %s", err.Error())
-
-            return
-
-        }
-
-        vmData, err := printVirtualMachineDetails([]byte(vmJSON))
-
-        // fmt.Println(vmData)
-
-        if err != nil {
-
-            fmt.Println("Error:", err)
-
-            return
-
-        }
-
-        actual_provisioningState, ok := vmData["properties"].(map[string]interface{})["provisioningState"].(string)
-
-        // fmt.Println(actual_provisioningState)
-
-        if !ok {
-
-            t.Errorf("Failed to get provisioning state for VM %s", name)
-
-            return
-
-        }
-
-
-
-
-        t.Run(fmt.Sprintf("State has been matched for VM: %s", name), func(t *testing.T) {
-
-            assert.Equal(t, expected_provisioningState, actual_provisioningState, fmt.Sprintf("State has been mis-matched for VM: %s", name))
-
-        })
-
-
-
-
-    }
-
-
+	}
 
 }
-
-
-
 
 func getAccessToken(subscriptionID string) (string, error) {
 
-    cmd := exec.Command("az", "account", "get-access-token", "--query", "accessToken", "--output", "tsv", "--subscription", subscriptionID)
+	cmd := exec.Command("az", "account", "get-access-token", "--query", "accessToken", "--output", "tsv", "--subscription", subscriptionID)
 
+	output, err := cmd.Output()
 
+	if err != nil {
 
+		return "", err
 
-    output, err := cmd.Output()
+	}
 
-    if err != nil {
-
-        return "", err
-
-    }
-
-
-
-
-    return strings.TrimSpace(string(output)), nil
+	return strings.TrimSpace(string(output)), nil
 
 }
-
-
-
-
-
 
 // function to fetch the details of the VM..
 
 func getVirtualMachineDetails(url, accessToken string) (string, error) {
 
-    client := &http.Client{}
+	client := &http.Client{}
 
-    req, err := http.NewRequest(http.MethodGet, url, nil)
+	req, err := http.NewRequest(http.MethodGet, url, nil)
 
-    if err != nil {
+	if err != nil {
 
-        return "", err
+		return "", err
 
-    }
+	}
 
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", accessToken))
 
+	resp, err := client.Do(req)
 
+	if err != nil {
 
-    req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", accessToken))
+		return "", err
 
+	}
 
+	defer resp.Body.Close()
 
+	body, err := ioutil.ReadAll(resp.Body)
 
-    resp, err := client.Do(req)
+	if err != nil {
 
-    if err != nil {
+		return "", err
 
-        return "", err
+	}
 
-    }
-
-    defer resp.Body.Close()
-
-
-
-
-    body, err := ioutil.ReadAll(resp.Body)
-
-    if err != nil {
-
-        return "", err
-
-    }
-
-
-
-
-    return string(body), nil
+	return string(body), nil
 
 }
-
-
-
-
-
 
 func printVirtualMachineDetails(vmJSON []byte) (map[string]interface{}, error) {
 
-    var vmData map[string]interface{}
+	var vmData map[string]interface{}
 
-    err := json.Unmarshal(vmJSON, &vmData)
+	err := json.Unmarshal(vmJSON, &vmData)
 
-    if err != nil {
+	if err != nil {
 
-        fmt.Printf("Failed to unmarshal JSON response: %s\n", err.Error())
+		fmt.Printf("Failed to unmarshal JSON response: %s\n", err.Error())
 
-        return nil, err
+		return nil, err
 
-    }
+	}
 
-
-
-
-    return vmData, nil
+	return vmData, nil
 
 }
-
-
